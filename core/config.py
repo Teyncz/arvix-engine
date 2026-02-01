ee897684348965069d4319d4d5bbf43c4dd6f2d0
Thibault Roth <thibault.roth@etu.unistra.fr>
2026-02-01 16:38:04 +0100

    add config.py for database configuration and update .gitignore

---
+++ core/config.py
@@ -0,0 +1,11 @@
+import os
+
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+SYNC_API_SECRET = os.getenv("SYNC_API_SECRET")
