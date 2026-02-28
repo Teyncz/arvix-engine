d830468e83237d146ec2163c1e45540500afdaae
Thibault Roth <thibault.roth@etu.unistra.fr>
2026-02-28 12:08:38 +0100

    refactor: update database connection import and remove unused front-end session

--- database/dependencies.py
+++ database/dependencies.py
@@ -1,16 +1,9 @@
-from connection import SessionLocal, FrontSessionLocal
+from database.connection import SessionLocal
 from sqlalchemy.orm import Session
 from typing import Generator
 
 def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-def get_front_db() -> Generator[Session, None, None]:
-    db = FrontSessionLocal()
     try:
         yield db
     finally:
